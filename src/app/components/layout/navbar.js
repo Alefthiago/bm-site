6001db8a1acf6e6aa815998f7d4b6a22b9921529
alefthiago <alefsilvadn@gmail.com>
2024-06-12 14:00:14 -0300

    ajustando layout do projeto.

---
+++ src/app/components/layout/navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <section>
+                <div>
+                    
+                </div>
+                <div>
+                    
+                </div>
+            </section>
+        </nav>
+    );
+};
+
+export default Navbar;
