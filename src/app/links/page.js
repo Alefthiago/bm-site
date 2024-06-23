f3d58e31d5d1024bf8aef995dc23447ddec556f8
alefthiago <alefsilvadn@gmail.com>
2024-06-23 12:04:38 -0300

    att nav bar.

---
+++ src/app/links/page.js
@@ -0,0 +1,13 @@
+// Util //
+import Globals from "@/components/globals";
+// Util. //
+
+const pageLinks = () => {
+    return (
+        <div className={`${Globals.default_style_page}`}>
+            <h1>Links</h1>
+        </div>
+    );
+}
+
+export default pageLinks;
