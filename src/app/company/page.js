03ed1d2b8cb699f34a60b3a17d5246c0aeca6cf3
FabioJro <frjcont@gmail.com>
2024-07-09 08:07:45 -0300

    Ajustes footer

--- src/app/company/page.js
+++ src/app/company/page.js
@@ -4,7 +4,7 @@ import Globals from "@/components/globals";
 
 let h3_text = "text-5xl font-bold text-center";
 let titulo = "text-xl font-bold text-center";
-let informativo = "text-[#208da5]";
+let informativo = "text-[#208da5]  md:text-justify";
 let info = "text-[#208da5] text-sm";
 
 
@@ -23,11 +23,11 @@ const pageCompany = () => {
             </section>
 
             
-            <section className="flex flex-col md:flex-row justify-between pt-12 w-full max-w-screen-lg">
+            <section className="flex flex-row justify-between pt-12 w-full max-w-screen-lg">
                 
-                <section className="text-center md:text-left pt-12 md:pr-2 w-full md:w-1/3">
+                <section className="text-justify w-1/3 pt-10">
                     <div className="pl-5 flex flex-col justify-center items-center gap-5">
-                        <div className="border-2 bg-orange-600 rounded-full w-12 h-12 flex justify-center items-center">
+                        <div className="border-2 bg-[#A55820] rounded-full w-12 h-12 flex justify-center items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-star-fill text-[#ffffff]" viewBox="0 0 16 16">
                                 <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
                             </svg>
@@ -40,9 +40,9 @@ const pageCompany = () => {
                 </section>
 
              
-                <section className="text-center md:text-left pt-12 md:pl-2 w-full md:w-1/3">
+                <section className="text-justify w-1/3">
                     <div className="pl-5 flex flex-col justify-center items-center gap-5">
-                        <div className="border-2 bg-orange-600 rounded-full w-12 h-12 flex justify-center items-center">
+                        <div className="border-2 bg-[#A55820] rounded-full w-12 h-12 flex justify-center items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye-fill text-[#ffffff]" viewBox="0 0 16 16">
                                 <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
                                 <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7" />
@@ -56,9 +56,9 @@ const pageCompany = () => {
                 </section>
 
                 
-                <section className="text-justify md:text-left pt-12 md:pl-2 w-full md:w-1/3">
+                <section className="text-justify w-1/3 pt-10">
                     <div className="pl-5 flex flex-col justify-center items-center gap-5">
-                        <div className="border-2 bg-orange-600 rounded-full w-12 h-12 flex justify-center items-center">
+                        <div className="border-2 bg-[#A55820] rounded-full w-12 h-12 flex justify-center items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bar-chart-fill text-[#ffffff]" viewBox="0 0 16 16">
                                 <path d="M1 11a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1zm5-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1z" />
                             </svg>
