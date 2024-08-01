8bf9e02e87cb89ac21eb304491f32291a17c5fd9
alefthiago <alefsilvadn@gmail.com>
2024-08-01 18:00:50 -0300

    Adicionando animações.

--- src/app/page.js
+++ src/app/page.js
@@ -1,63 +1,81 @@
+'use client';
 // UTIL //
 import Image from "next/image";
 import Globals from "@/components/globals";
 import Contacts from "@/components/contacts";
+import { useEffect } from "react";
 // UTIL. //
 
 export default function Home() {
+  useEffect(() => {
+    const obeserver = new IntersectionObserver((e) => {
+      // console.log(e)
+      e[0].target.classList.add('init-hidden-off');
+    }, {
+      threshold: 1
+    });
+
+    // Array.from(document.querySelectorAll('fade-in'));
+    Array.from(document.querySelectorAll('.init-hidden')).forEach(elemento => {
+      obeserver.observe(elemento);
+    });
+    // obeserver.observe(document.querySelector('.init-hidden'));
+
+
+    // if(typeof document !== 'undefined' && typeof window !== 'undefined') {   
+    // }
+  }, []);
   return (
     <section className={`${Globals.default_style_page}`}>
-      <section className={`w-full`}> {/* xl:h-[70vh] */}
-
-        <div className={`w-full lg:flex gap-2`}>
-          <div className={`l:w-1/2 w-full`}>
-            <h1 className={`lg:text-left text-center lg:text-6xl text-4xl roboto-bold text-[#0E0E0E]`}>
+      <section > {/* xl:h-[70vh] */}
+        <div className={`init-hidden`}>
+          <div>
+            <h1 className={`text-center md:text-6xl text-4xl roboto-bold text-[#0E0E0E]`}>
               Encontre a
               Melhor
               Solução Para o
               Negócio Aqui
             </h1>
-            <p className={`lg:text-left text-center lg:text-2xl text:xl raleway-medium text-[#316994] pt-5`}>
-              Nossos profissionais,
-              especialistas em várias áreas
-              criam soluções que
-              transformam seu dia a dia.
-            </p>
+            <div className={`pl-10 pr-10`}>
+              <p className={`text-center md:text-2xl text-[18px] raleway-medium text-[#316994] pt-5`}>
+                Nossos profissionais,
+                especialistas em várias áreas
+                criam soluções que
+                transformam seu dia a dia.
+              </p>
+            </div>
           </div>
 
-          <div className={`lg:w-1/2 w-full flex items-center justify-center pt-5`}>
+          <div className={`md:m-w-[500px] md:m-h-[300px] w-full flex items-center justify-center md:pt-24 pt-10 pr-2 pl-2`}>
             <div>
               <Image
-                src={'./BM.png'}
+                src={'./BM.svg'}
                 alt="Logo BMinformatica"
-                width={300}
-                height={300}
-                className="lg:w-[500px] lg:h-[500px]"
+                width={600}
+                height={600}
               />
             </div>
           </div>
         </div>
 
-        <div className={`w-full h-[250px] text-center lg:text-2xl text-xl raleway-medium flex-col items-center pt-10`}>
-
-          <div className={`w-full`}>
+        <div className={`w-full h-[250px] text-center md:text-2xl text-[18px] raleway-medium flex-col items-center md:pt-32 pt-20`}>
+          <div className={`init-hidden w-full`}>
             <h3 className={`text-[#316994]`}>
               Sistemas ERP/PAF-ECF/NFC-E Automação Comercial <br />
               Desenvolvimento Desktop, Web e Mobile.
             </h3>
           </div>
 
-          <div className={`w-full pt-5`}>
+          <div className={`init-hidden w-full pt-5`}>
             <h3 className={`text-[#208DA5]`}>
               Entre em contato conosco, solicite uma demonstração e faça um orçamento
             </h3>
           </div>
-
         </div>
       </section>
 
-      <section className={`w-full`}>
-        <Contacts/>
+      <section className={`init-hidden w-full md:pt-32 pt-20`}>
+        <Contacts />
         {/* <Image
         src="/vercel.svg"
         alt="Vercel Logo"
--- src/components/contacts/index.js
+++ src/components/contacts/index.js
@@ -7,15 +7,15 @@ const Contacts = () => {
 
     return (
         <>
-            <div className={`w-full pt-10 flex items-center gap-5`}>
+            <div className={`w-full flex items-center gap-5`}>
                 <div className={`h-[5px] w-[60%] bg-[#A55820] rounded-[7px]`}></div>
                 <div className="w-[40%] text-center">
-                    <h1 className={`lg:text-3xl text-3xl roboto-bold`}>Fones</h1>
+                    <h1 className={`md:text-3xl text-2xl roboto-bold`}>Fones</h1>
                 </div>
                 <div className={`h-[5px] w-[60%] bg-[#A55820] rounded-[7px]`}></div>
             </div>
 
-            <div className={'w-full pt-5 grid lg:grid-cols-4 grid-cols-2 gap-4 place-content-center text-center lg:text-2xl text-xl text-[#316994] roboto-bold'}>
+            <div className={'w-full md:pt-10 pt-5 pb-5 grid md:grid-cols-4 grid-cols-2 gap-4 place-content-center text-center md:text-2xl text-[#316994] roboto-bold'}>
                 <div>
                     Pernambuco <br />
                     (81) 3126-2050
