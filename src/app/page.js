c96066b1ce7f5633d114e72799bf0f2718d6bf1a
Alef Thiago da Silva <112173530+Alefthiago@users.noreply.github.com>
2024-08-02 06:42:28 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -37,12 +37,11 @@ export default function Home() {
   return (
     <section className={`${Globals.default_style_page}`}>
 <header className="bg-white dark:bg-gray-900">
-
     <div className="container px-6 py-16 mx-auto">
         <div className="items-center lg:flex">
             <div className="w-full lg:w-1/2">
                 <div className="lg:max-w-lg">
-                    <h1 className="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Best place to choose <br> your <span class="text-blue-500 ">clothes</span></h1>
+                    <h1 className="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Best place to choose <br> your <span className="text-blue-500 ">clothes</span></h1>
                     
                     <p className="mt-3 text-gray-600 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro beatae error laborum ab amet sunt recusandae? Reiciendis natus perspiciatis optio.</p>
                     
