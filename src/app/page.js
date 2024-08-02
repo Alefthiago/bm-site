12c19246b15280d43386e4f02cf7c51d967fd3c0
Alef Thiago da Silva <112173530+Alefthiago@users.noreply.github.com>
2024-08-02 06:59:32 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -46,11 +46,17 @@ export default function Home() {
               Solução Para o Seu
               Negócio
   <span className="text-blue-500 ">
-  Aqui
+   Aqui
   </span>
   </h1>
                     
-                    <p className="mt-3 text-gray-600 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro beatae error laborum ab amet sunt recusandae? Reiciendis natus perspiciatis optio.</p>
+                    <p className="mt-3 text-gray-600 dark:text-gray-400">
+                Nossos profissionais,
+                especialistas em várias áreas
+                criam soluções que
+                transformam seu dia a dia com Sistemas ERP/PAF-ECF/NFC-E Automação Comercial
+                Desenvolvimento Desktop, Web e Mobile.
+                </p>
                     
                     <button className="w-full px-5 py-2 mt-6 text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-blue-600 rounded-lg lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Shop Now</button>
                 </div>
