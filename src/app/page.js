6562fe760a678b13f74ee28f8dcca7b03e7c10a5
Alef Thiago da Silva <112173530+Alefthiago@users.noreply.github.com>
2024-08-02 06:53:41 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -37,7 +37,7 @@ export default function Home() {
   return (
     <section className={`${Globals.default_style_page}`}>
 <header className="bg-white dark:bg-gray-900">
-    <div className="container px-6 py-16 mx-auto">
+    <div className="container px-6 mx-auto">
         <div className="items-center lg:flex">
             <div className="w-full lg:w-1/2">
                 <div className="lg:max-w-lg">
