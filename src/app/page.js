ffaf7336fcd20105c9451d5256276afe1c1971b5
Alef Thiago da Silva <112173530+Alefthiago@users.noreply.github.com>
2024-08-02 07:04:23 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -44,7 +44,7 @@ export default function Home() {
                     <h1 className="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">
   A Melhor
               Solução Para o Seu
-              Negócio
+              Negócio 
   <span className="text-blue-500 ">
    Aqui
   </span>
@@ -57,7 +57,27 @@ export default function Home() {
                 transformam seu dia a dia com Sistemas ERP/PAF-ECF/NFC-E Automação Comercial
                 Desenvolvimento Desktop, Web e Mobile.
                 </p>
-                    
+                    <div>
+                  <button className={`button`}>
+                  <div className="svg-wrapper-1">
+                    <div className="svg-wrapper">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        width="30"
+                        height="30"
+                      >
+                        <path fill="none" d="M0 0h24v24H0z"></path>
+                        <path
+                          fill="currentColor"
+                          d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
+                        ></path>
+                      </svg>
+                    </div>
+                  </div>
+                  <span className={`roboto-medium`}>Contratar</span>
+                </button>
+                  </div>
                     <button className="w-full px-5 py-2 mt-6 text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-blue-600 rounded-lg lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Shop Now</button>
                 </div>
             </div>
