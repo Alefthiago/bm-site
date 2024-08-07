1edb61447b689cd3169cad6f24e874b205cc6521
alefthiago <alefsilvadn@gmail.com>
2024-08-07 12:26:43 -0300

    add imgs

--- src/app/equipment/page.js
+++ src/app/equipment/page.js
@@ -8,56 +8,65 @@ const pageEquipment = () => {
     const cardsData = [
         {
             alt: "Mini Impressoras",
-            imgSrc: "../../imgEq/R.jpeg",
-            link: "https://www.exemplo1.com",
+            imgSrc: "./09e0add64a2c83e9bafd0d39f8707652.png",
+            // link: "https://www.exemplo1.com",
+            hover: true,
             text: "Mini Impressoras"
         },
         {
             imgAlt: "Descrição da imagem 2",
-            imgSrc: "../../imgEq/R.jpeg",
-            link: "https://www.exemplo2.com",
+            imgSrc: "./3500c1_42c81e47b8064624bc1407bdcc39cfd8~mv2.webp",
+            // link: "https://www.exemplo2.com",
+            hover: true,
             text: "Smart PCs"
         },
         {
             imgAlt: "Descrição da imagem 2",
-            imgSrc: "../../imgEq/R.jpeg",
-            link: "https://www.exemplo2.com",
+            imgSrc: "./3500c1_9ef9944dc52b4cb7ab5cb325051d1b4e~mv2.webp",
+            // link: "https://www.exemplo2.com",
+            hover: true,
             text: "TEF (Transferência Eletrônica de Fundos) / Pin-Pads"
         },
         {
             imgAlt: "Descrição da imagem 2",
-            imgSrc: "../../imgEq/R.jpeg",
-            link: "https://www.exemplo2.com",
+            imgSrc: "./3500c1_8f961a30ca144868af811f2014b50fa7~mv2.webp",
+            // link: "https://www.exemplo2.com",
+            hover: true,
             text: "Leitores de Código de Barras"
         },
         {
             imgAlt: "Descrição da imagem 2",
-            imgSrc: "../../imgEq/R.jpeg",
-            link: "https://www.exemplo2.com",
+            imgSrc: "./3500c1_b4cccac9b8c54e31b77c42d11116f46e~mv2.webp",
+            // link: "https://www.exemplo2.com",
+            hover: true,
             text: "Gavetas de Dinheiro"
         },
         {
             imgAlt: "Descrição da imagem 2",
-            imgSrc: "../../imgEq/R.jpeg",
-            link: "https://www.exemplo2.com",
+            imgSrc: "./3500c1_661759b7fe76411c8045f17a26a61f4e~mv2.webp",
+            // link: "https://www.exemplo2.com",
+            hover: true,
             text: "Impressora de Etiquetas de Código de Barras"
         },
         {
             imgAlt: "Descrição da imagem 2",
-            imgSrc: "../../imgEq/R.jpeg",
-            link: "https://www.exemplo2.com",
+            imgSrc: "./3500c1_6011ee0c63b04398b6c9349e91b6484a~mv2.webp",
+            // link: "https://www.exemplo2.com",
+            hover: true,
             text: "CPUs de Varejo"
         },
         {
             imgAlt: "Descrição da imagem 2",
-            imgSrc: "../../imgEq/R.jpeg",
-            link: "https://www.exemplo2.com",
+            imgSrc: "./3500c1_34bcb7f538734f428ae61c20a96bb3dc~mv2.webp",
+            // link: "https://www.exemplo2.com",
+            hover: true,
             text: "Monitores"
         },
         {
             imgAlt: "Descrição da imagem 2",
-            imgSrc: "../../imgEq/R.jpeg",
-            link: "https://www.exemplo2.com",
+            imgSrc: "./3500c1_5d7bf9e82a2644308a63d240dc2f316b~mv2.webp",
+            // link: "https://www.exemplo2.com",
+            hover: true,
             text: "Balanças Eletrônicas"
         }
 
