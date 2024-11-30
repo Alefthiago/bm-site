bc86f4ac0907ce92f58c2891837597c8ef11d8e0
alefthiago <alefsilvadn@gmail.com>
2024-11-30 09:33:01 -0300

    ajusttes

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -38,7 +38,7 @@ const Cardes = (props) => {
             <div className="container px-6 py-10 mx-auto">
                 <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
                     {props.cardsData.map((card, index) => (
-                        <div key={index} className={`invisible ${index <= 2 || (index > 5 && index < 9) ? 'show-right' : 'show-left'} w-full max-w-sm bg-white rounded-lg shadow-lg hover:scale-105 transition-transform duration-500 ease-in-out hover:cursor-pointer`}>
+                        <div key={index} className={`invisible ${index <= 2 || (index > 5 && index < 9) ? 'show-right' : 'show-left'} drop-shadow-xl w-full max-w-sm bg-white rounded-lg shadow-lg hover:scale-105 transition-transform duration-500 ease-in-out hover:cursor-pointer`}>
                             <img className="object-cover object-center w-full h-[310px] p-2" src={card.imgSrc} alt="avatar" />
 
                             <div className="px-6 py-4 min-h-[300px]">
@@ -49,7 +49,7 @@ const Cardes = (props) => {
 
                                 <div className="grid grid-cols-1 md:grid-cols-2">
                                     {Array.isArray(card.nome) && card.nome.map((item, index) => (
-                                        <div key={index} className={`flex items-center mt-4 text-gray-700`}>
+                                        <div key={index} className={`flex items-center mt-4 text-gray-700 rounded-lg`}>
                                             <svg aria-label="suitcase icon" className="w-6 h-6 fill-current" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M14 11H10V13H14V11Z" />
                                                 <path fillRule="evenodd" clipRule="evenodd" d="M7 5V4C7 2.89545 7.89539 2 9 2H15C16.1046 2 17 2.89545 17 4V5H20C21.6569 5 23 6.34314 23 8V18C23 19.6569 21.6569 21 20 21H4C2.34314 21 1 19.6569 1 18V8C1 6.34314 2.34314 5 4 5H7ZM9 4H15V5H9V4ZM4 7C3.44775 7 3 7.44769 3 8V14H21V8C21 7.44769 20.5522 7 20 7H4ZM3 18V16H21V18C21 18.5523 20.5522 19 20 19H4C3.44775 19 3 18.5523 3 18Z" />
@@ -63,10 +63,12 @@ const Cardes = (props) => {
                                 </div>
                             </div>
 
-                            <div className="flex items-center px-6 py-3 bg-gray-900 justify-center">
+                            <div className="flex items-center px-6 py-3 bg-[#206BA5] justify-center rounded-b-lg">
                                 <a href="#" target="_blank">
-                                    <button className="px-2 py-1 text-xs inter-bold text-gray-900 uppercase transition-colors duration-300 transform bg-white rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">
-                                        Mais Informações
+                                    <button className="px-2 py-1 text-sm uppercase text-[#F8F8F8]">
+                                        <span className="font-semibold font-sans">
+                                            Mais Informações
+                                        </span>
                                     </button>
                                 </a>
                             </div>
--- src/components/navbar/menuMobile.js
+++ src/components/navbar/menuMobile.js
@@ -17,14 +17,13 @@ const MenuMobile = (props) => {
 
     return (
         <>
-            <Drawer open={props.is_open} onClose={handleClose} position="right" className='roboto-bold bg-[#F8F8F8]'>
+            <Drawer open={props.is_open} onClose={handleClose} position="right" className='roboto-bold'>
                 <Drawer.Header title="Menu" className="font-mono" />
                 <Drawer.Items>
                     <div className="font-sans grid grid-cols-1 gap-4 md:grid-cols-1">
                         <Link
                             onClick={() => {
                                 props.clickIcon('home', 'Inicio');
-                                // props.randleRotation(); 
                             }}
                             href={'/'}
                             className={`w-full rounded-lg border border-gray-200  px-4 py-2  text-[#0E0E0E] text-center text-sm ${props.verifyRoute('/', true) ? '' : 'hover:bg-gray-100 hover:text-[#208DA5]'} ${props.verifyRoute('/', true)} focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200`}
@@ -68,7 +67,6 @@ const MenuMobile = (props) => {
                         </Link>
 
                         <Link
-                            // onClick={() => props.clickIcon('/suport', 'suport')}
                             href={'https://wa.me/558131262050'}
                             target='_blank'
                             className={`w-full rounded-lg inline-flex justify-center items-center bg-cyan-700 border border-gray-200 px-4 py-2 text-gray-200 text-center text-sm focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200`}
@@ -79,29 +77,6 @@ const MenuMobile = (props) => {
                             &nbsp;
                             Contato
                         </Link>
-
-                        {/* <a
-                            href="https://wa.me/558131262050"
-                            target='_blank'
-                            className="inline-flex items-center rounded-lg bg-cyan-700 px-4 py-2 text-center text-sm text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300"
-                        >
-                            Contato&nbsp;
-                            <svg
-                                className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 14 10"
-                            >
-                                <path
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                                />
-                            </svg>
-                        </a> */}
                     </div>
                 </Drawer.Items>
             </Drawer>
