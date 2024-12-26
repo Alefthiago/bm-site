5d120a7ff82eff8917421d421c3ee059a148b501
alefthiago <alefsilvadn@gmail.com>
2024-12-26 09:45:54 -0300

    Ajuste pagina 404.

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -22,12 +22,18 @@ const Custom404 = () => {
                 <section className="lg:w-3/5 mx-auto">
                     <div className="container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12">
                         <div className="wf-ull lg:w-1/2">
-                            <p className="text-sm font-medium text-blue-500">Erro 404</p>
-                            <h1 className="mt-3 text-2xl font-semibold text-gray-800 md:text-3xl">Página não encontrada</h1>
-                            <p className="mt-4 text-gray-500">Desculpe, a página que você está procurando não existe. Aqui estão alguns links úteis:</p>
+                            <p className="text-sm font-medium text-blue-500">
+                                Erro 404
+                            </p>
+                            <h1 className="mt-3 text-2xl font-semibold text-gray-800 md:text-3xl">
+                                Página não encontrada
+                            </h1>
+                            <p className="mt-4 text-gray-500">
+                                Desculpe, a página que você está procurando não existe.
+                            </p>
 
                             <div className="flex items-center mt-6 gap-x-3">
-                                <Link href={`/bm-site`} className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600">
+                                <Link href={`/`} className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600">
                                     Ir para a página inicial
                                 </Link>
                             </div>
