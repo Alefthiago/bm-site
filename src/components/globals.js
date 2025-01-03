a0a9a1bedcae1da8fb61e5f523dc89980d845cfe
alefthiago <alefsilvadn@gmail.com>
2025-01-03 13:52:30 -0300

    Ajuste função obsever

--- src/components/globals.js
+++ src/components/globals.js
@@ -1,17 +1,22 @@
 export default class Globals {
     static default_style_page = "flex min-h-screen flex-col items-center pt-10 pb-2";
     static observer() {
-        const observer = new IntersectionObserver((entries) => {
+        let observer = new IntersectionObserver((entries) => {
             entries.forEach((entry) => {
                 if (entry.intersectionRatio > 0) {
-                    if (entry.target.classList.contains('show-top')) {
-                        entry.target.classList.add('animate-fade-up');
-                    } else if (entry.target.classList.contains('show-bottom')) {
-                        entry.target.classList.add('animate-fade-down');
-                    } else if (entry.target.classList.contains('show-left')) {
-                        entry.target.classList.add('animate-fade-left');
-                    } else {
-                        entry.target.classList.add('animate-fade-right');
+                    switch (true) {
+                        case entry.target.classList.contains('show-top'):
+                            entry.target.classList.add('animate-fade-up');
+                            break;
+                        case entry.target.classList.contains('show-bottom'):
+                            entry.target.classList.add('animate-fade-down');
+                            break;
+                        case entry.target.classList.contains('show-left'):
+                            entry.target.classList.add('animate-fade-left');
+                            break;
+                        case entry.target.classList.contains('show-right'):
+                            entry.target.classList.add('animate-fade-right');
+                            break;
                     }
                     entry.target.classList.remove('invisible');
                 }
@@ -19,13 +24,13 @@ export default class Globals {
         }, {
             threshold: [0]
         });
-
-        Array.from(document.querySelectorAll('.show-top, .show-bottom, .show-left, .show-right')).forEach(element => {
+    
+        document.querySelectorAll('.show-top, .show-bottom, .show-left, .show-right').forEach(element => {
             observer.observe(element);
         });
-
+    
         return () => {
             observer.disconnect();
         };
-    }
+    }    
 }
