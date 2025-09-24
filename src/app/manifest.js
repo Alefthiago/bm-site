c0478f28e0280086cf8fac7d468abf22d68f6583
alefthiago <alefsilvadn@gmail.com>
2025-09-24 14:10:20 -0300

    Teste de pwa.

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -9,34 +9,14 @@ export default function manifest() {
     theme_color: '#000000',
     icons: [
       {
-        src: 'https://cdn.jsdelivr.net/npm/@primer/octicons@17.7.0/build/svg/mark-github-16.svg',
-        sizes: '16x16',
-        type: 'image/svg+xml',
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
       },
       {
-        src: 'https://cdn.jsdelivr.net/npm/@primer/octicons@17.7.0/build/svg/mark-github-32.svg',
-        sizes: '32x32',
-        type: 'image/svg+xml',
-      },
-      {
-        src: 'https://cdn.jsdelivr.net/npm/@primer/octicons@17.7.0/build/svg/mark-github-64.svg',
-        sizes: '64x64',
-        type: 'image/svg+xml',
-      },
-      {
-        src: 'https://cdn.jsdelivr.net/npm/@primer/octicons@17.7.0/build/svg/mark-github-128.svg',
-        sizes: '128x128',
-        type: 'image/svg+xml',
-      },
-      {
-        src: 'https://cdn.jsdelivr.net/npm/@primer/octicons@17.7.0/build/svg/mark-github-256.svg',
-        sizes: '256x256',
-        type: 'image/svg+xml',
-      },
-      {
-        src: 'https://cdn.jsdelivr.net/npm/@primer/octicons@17.7.0/build/svg/mark-github-512.svg',
+        src: '/icon-512x512.png',
         sizes: '512x512',
-        type: 'image/svg+xml',
+        type: 'image/png',
       },
     ],
   };
